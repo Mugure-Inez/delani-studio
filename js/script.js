a2d3e4ec38d897713ed059c49329c938e7c1230b
Mugure-Inez <enezmugure@gmail.com>
2021-02-28 22:32:12 +0300

    changes made to js and service section

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+    $("#dev-icon").click(function(){
+      $("#development-image").slideDown('1500').hide('1000');
+      $("#development").show('1500');
+    });
+    $("#development").click(function(){
+      $("#development").slideUp('1500');
+      $("#dev-icon").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#design-icon").click(function(){
+      $("#design-icon").slideDown('1500').hide('1000');
+      $("#design").show('1500');
+    });
+    $("#design").click(function(){
+      $("#design").slideUp('1500');
+      $("#design-icon").slideDown('1500');
+    });
+  });
+  
+  $(document).ready(function(){
+    $("#product-icon").click(function(){
+      $("#product-icon").slideDown('1500').hide('1000');
+      $("#product").show('1500');
+    });
+    $("#product").click(function(){
+      $("#product").slideUp('1500');
+      $("#product-icon").slideDown('1500');
+    });
+  });
